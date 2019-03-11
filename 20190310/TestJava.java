f7679b8e318bc84c4bb9c5271a23f1226bfb34a9
Luke <lukelu_ajk@58ganji.com>
2019-03-11 19:08:26 +0800

    规范

--- 20190310/TestJava.java
+++ 20190310/TestJava.java
@@ -1,12 +1,21 @@
-
+/**
+ * TestJava
+ *
+ * @author Luke
+ * @date 2019/03/11
+ */
 public class TestJava {
 
     public static void main(String[] args) {
 
-        int num = 10;//定义整型变量
-        num = 30;//修改变量内容
-        System.out.println("num 内容是:" + num);//输出内容
-        System.out.println("num * num = " + num * num);//输出乘方内容
+        //定义整型变量
+        int num = 10;
+        //修改变量内容
+        num = 30;
+        //输出内容
+        System.out.println("num 内容是:" + num);
+        //输出乘方内容
+        System.out.println("num * num = " + num * num);
 
     }
 }
