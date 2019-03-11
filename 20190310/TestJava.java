32b8fdaf618c8a59735b05291756c541609a686e
Luke <lukelu_ajk@58ganji.com>
2019-03-11 09:57:42 +0800

    添加练习内容

---
+++ 20190310/TestJava.java
@@ -0,0 +1,12 @@
+
+public class TestJava {
+
+    public static void main(String[] args) {
+
+        int num = 10;//定义整型变量
+        num = 30;//修改变量内容
+        System.out.println("num 内容是:" + num);//输出内容
+        System.out.println("num * num = " + num * num);//输出乘方内容
+
+    }
+}
--- 20190310/hello.java
+++ 20190310/hello.java
@@ -1,7 +1,7 @@
 
-public class Hello{
+public class Hello {
 
-    public  static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("The java word hy14030034");
     }
 }
